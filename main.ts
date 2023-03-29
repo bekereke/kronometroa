b0dc238078e0dbac3d3bb17d701144da927df2dc
Aitor Aretxaga <aitoraretxaga@protonmail.com>
2023-03-29 12:55:18 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,21 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + segunduak + "' " + segundu_ehunenak + "\"")
 })
+input.onButtonPressed(Button.AB, function () {
+    AB_sakatuta = true
+})
 input.onButtonPressed(Button.B, function () {
     basic.showString("clear")
     segunduak = 0
     segundu_ehunenak = 0
 })
+let AB_sakatuta = false
 let segunduak = 0
 let segundu_ehunenak = 0
 basic.clearScreen()
 segundu_ehunenak = 0
 segunduak = 0
+AB_sakatuta = true
 loops.everyInterval(1000, function () {
     segunduak += 1
     if (segunduak == 60) {
